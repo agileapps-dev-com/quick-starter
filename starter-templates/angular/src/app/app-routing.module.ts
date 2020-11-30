bd6b742509e364568be1938edba24bd29fbc8d85
Kizhakkarakkatt Dominic <ski@softwareag.com>
2020-11-30 18:00:35 +0530

    update location strategy to use hash

--- starter-templates/angular/src/app/app-routing.module.ts
+++ starter-templates/angular/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, { path: '', redirectTo: "login", pathMatch: "full" }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
